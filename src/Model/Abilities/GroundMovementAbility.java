656fd81fbb0efd941fd358a5b421b184f7f19c63
Trevor Yovaish <tyovaish13@gmail.com>
2017-04-15 15:51:34 -0400

    Working on model and controller now

--- src/Model/Abilities/GroundMovementAbility.java
+++
@@ -1,8 +0,0 @@
-package Model.Abilities;
-
-/**
- * Created by Trevor on 4/14/2017.
- */
-public class GroundMovementAbility extends MovementAbility {
-
-}
---
+++ src/Model/Resource/ResourceManager.java
@@ -0,0 +1,7 @@
+package Model.Transportation;
+
+/**
+ * Created by Trevor on 4/15/2017.
+ */
+public class ResourceManager {
+}
