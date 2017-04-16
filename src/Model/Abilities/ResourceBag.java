d48a2d6c623dbfef9707c2dfa825b879eb8b5388
Trevor Yovaish <tyovaish13@gmail.com>
2017-04-15 20:53:50 -0400

    Resource stuff, keeping it real

---
+++ src/Model/Abilities/ResourceBag.java
@@ -0,0 +1,14 @@
+package Model.Abilities;
+
+import Model.Resource.Resource;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Trevor on 4/15/2017.
+ */
+public class CarryResourceAbility {
+
+    ArrayList<Resource> resourceBag;
+
+}
