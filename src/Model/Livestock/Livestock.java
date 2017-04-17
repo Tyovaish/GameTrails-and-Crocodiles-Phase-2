cfbbbc14362355e9f1949d3a22888cac635c8221
larryelantz <umbranox@ufl.edu>
2017-04-17 04:16:10 -0400

    Update Livestock.java

--- src/Model/Livestock/Livestock.java
+++ src/Model/Livestock/Livestock.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
  * Created by Larry on 4/17/2017.
  */
 public class Livestock {
+    private int player;
     ArrayList<MovementAbility> movementAbilities;
 
     Livestock(){ movementAbilities = new ArrayList<MovementAbility>(); }
@@ -15,5 +16,9 @@ public class Livestock {
     public ArrayList<MovementAbility> getMovementSet(){
         return movementAbilities;
     }
+    
+    public void setPlayer(int player){this.player = player;}
+    
+    public int getPlayer(){return this.player;}
 
 }
